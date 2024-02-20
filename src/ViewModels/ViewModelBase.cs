56eac530a5a3d8aef9082ba27083254caabe77e2
Gabe Cornejo <gcornejo441@gmail.com>
2024-02-19 21:06:36 -0500

    Revisioning

---
+++ src/ViewModels/ViewModelBase.cs
@@ -0,0 +1,6 @@
+﻿using ReactiveUI;
+
+namespace GetnMethods.ViewModels;
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ src/Views/MainWindow.axaml.cs
@@ -0,0 +1,74 @@
+using Avalonia.ReactiveUI;
+
+using FluentAvalonia.UI.Windowing;
+
+using GetnMethods.Products;
+using GetnMethods.Products.ProductViewModels;
+
+
+using GetnMethods.ViewModels;
+
+using ReactiveUI;
+
+using System;
+
+using System.Threading.Tasks;
+
+namespace GetnMethods.Views;
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
+{
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        this.WhenActivated(
+            d =>
+            {
+                ViewModel?.ShowNotificationDialog.RegisterHandler(HandleNotificationDialog);
+            });
+    }
+
+    private async Task HandleNotificationDialog(InteractionContext<IDialogProduct, bool> interaction)
+    {
+        var currentDialog = ViewModel?.DialogCalled;
+        var notificationMessage = ViewModel?.NotificationMessage;
+
+        try
+        {
+            if (currentDialog == "Warning")
+            {
+                var warningDialog = new WarningDialogProduct("Warning", "Warning!", "You cannot return from this action are you sure you would like to continue?");
+                warningDialog.DataContext = interaction.Input;
+                var result = await warningDialog.ShowDialog<bool>(this);
+                interaction.SetOutput(result);
+
+            } else if (currentDialog == "Download")
+            {
+
+                var downloadDialog = new DownloadDialogViewModel("Download", "Downloading",notificationMessage);
+                downloadDialog.DataContext = interaction.Input;
+
+                var result = await downloadDialog.ShowDialog<bool>(this);
+                interaction.SetOutput(result);
+
+            }
+
+
+        }
+        catch (Exception ex)
+        {
+            LogError(ex);
+
+            interaction.SetOutput(false);
+        }
+    }
+
+    private void LogError(Exception ex)
+    {
+        var warningDialog = new WarningDialogProduct("Error", "Error!", ex.Message);
+        warningDialog.DataContext = warningDialog;
+
+        warningDialog.Show();
+    }
+}
